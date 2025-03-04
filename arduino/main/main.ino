c4ba3f6fcef488c8bc4d6a07f96c220c9564eb77
VelasquezE <132410679+VelasquezE@users.noreply.github.com>
2025-03-04 09:33:24 -0500

    Add required functionalities, organize functions

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,18 +1,22 @@
+int measurements = 0;
+
+unsigned long t0 = 0; //time of first pass in same oscillation
+unsigned long t1 = 0; // time of second pass in same oscillation
+
+int detectionCounterSameOscillation = 0; 
+
 // photoresistor
 const int photoresistorPin = A0;
 int photoresistorReading;
-const int threshold = 400;
-unsigned long t0 = 0;
-unsigned long t1 = 0;
-float halfPeriod = 0;
-int ii = 0; // counts pendulum detection
-
-//Avoiding detecting the pendulum twice
-unsigned long lastDetection = 0; //
-const unsigned long waitingTime = 50000; // [\mu s]
 
 // electromagnet
 const int electromagnetPin = 2; 
+
+bool detectPendulum(int photoresistorReading);
+bool waitToMeasure();
+float calculatePeriod();
+void initializeSetup();
+void holdReleasePendulum();
 
 void setup() {
   Serial.begin(9600);
@@ -21,35 +25,110 @@ void setup() {
 
 void loop() {
   photoresistorReading = analogRead(photoresistorPin);
-  
-  if (photoresistorReading < threshold)
+  void initializeSetup();
+
+  if (detectPendulum(photoresistorReading))
   {
-    if ((micros() - lastDetection) > waitingTime)
-    {
-      lastDetection = micros();
-      switch (ii){
+    if (waitToMeasure)
+    { 
+      switch (detectionCounterSameOscillation){
         case 0:
           t0 = micros();
-          ii++;
+          detectionCounterSameOscillation++;
           break;
         case 1:
           t1 = micros();
-          halfPeriod = (t1 - t0) / 1000.0; //[ms]
-          Serial.print("Half period: ");
-          Serial.println(halfPeriod, 3);
+
+          float period = calculatePeriod();
+          Serial.print("Period: ");
+          Serial.println(period, 3);
 
           // activate electromagnet
-          digitalWrite(electromagnetPin, HIGH);
-          delay(3000); //waits 2 seconds before realeasing the pendulum
-          digitalWrite(electromagnetPin, LOW);
+          holdReleasePendulum();
 
-          ii = 0;
+          detectionCounterSameOscillation = 0;
           break;
       }
     }
   }
 
+  stopMeasuring();
+
   delay(1);
 }
 
+bool detectPendulum(int photoresistorReading)
+{
+  const int threshold = 400;
+
+  if (photoresistorReading < threshold)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+bool waitToMeasure()
+{
+  /* Due to the speed of the measurements, avoids detecting the 
+     pendulum twice by waiting a set amount of time
+  */
+  static unsigned long lastDetection = 0;
+  const unsigned long waitingTime = 50000; // [\mu s]
+
+  if ((micros() - lastDetection) > waitingTime)
+  {
+    lastDetection = micros();
+    return true;
+  }
+
+  return false;
+}
+
+float calculatePeriod()
+{
+  static float period = 0;
+  period = (2 * (t1 - t0)) / 1e-6; // [s]
+
+  return period;
+}
+
+void initializeSetup()
+{
+  /* Holds the pendulum when the program starts
+    and releases it after 10s to start taking
+    measurements
+  */
+  static bool launchSystem = true;
+
+  if (launchSystem)
+  {
+    digitalWrite(electromagnetPin, HIGH);
+    delay(10000); 
+    digitalWrite(electromagnetPin, LOW);
+  }
+
+  launchSystem = false; 
+}
+
+void holdReleasePendulum()
+{
+  /* Holds whem measurement is over, waits, 
+     and releases again
+  */
+  digitalWrite(electromagnetPin, HIGH);
+  delay(3000); //waits 3 seconds before realeasing the pendulum
+  digitalWrite(electromagnetPin, LOW);
+
+}
+
+void stopMeasuring()
+{
+  const int repetitions = 10;
+  while (measurements = repetitions)
+  {
+    digitalWrite(electromagnetPin, LOW);
+  }
+}
 
